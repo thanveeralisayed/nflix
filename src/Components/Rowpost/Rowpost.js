9f96492350e26a9d0182285f2b9b378f3fdd0152
thanveeralisayed <thanveeralisayed@gmail.com>
2021-07-20 16:42:18 +0530

    added function for getting trailer keys

--- src/Components/Rowpost/Rowpost.js
+++ src/Components/Rowpost/Rowpost.js
@@ -1,14 +1,16 @@
 import React,{useEffect,useState} from 'react'
 import axios from '../../axios'
-import {Api_Key,fhdImage_url,smImage_url} from '../../Constants/Constants'
+import {Api_Key,baseUrl,smImage_url} from '../../Constants/Constants'
 import './Rowpost.css'
+import YouTube from 'react-youtube'
+
 
 function Rowpost(props) {
     const poster = props.poster_type;
-    
+    let trailerUrl = '';
 
     const [movies, setMovies] = useState([])
-    const [more, setMore] = useState([])
+    
     
 
 
@@ -17,13 +19,26 @@ function Rowpost(props) {
         console.log(props);
              
             axios.get(props.ctype).then((Response)=>{
-                setMovies(Response.data.results)  
+                setMovies(Response.data.results)
+                  
             })
          
 
     }, []);
 
-
+    const handleMovieClick = (id) => {
+       
+        const url = baseUrl + `movie/${id}/videos?api_key=${Api_Key}&language=en-US`;
+        axios.get(url).then((Response)=>{
+            const vids = Response.data.results[0]; 
+            if(vids)
+            {
+                console.log(vids.key);
+                
+            }
+        })
+         
+    }
     
 
 
@@ -34,22 +49,20 @@ function Rowpost(props) {
 
                 {
                    props.original? movies.map((movie,pos)=>
-                   <div className="poster-box"><img key={pos} 
+                   <div onClick={()=>handleMovieClick(movie.id)} className="poster-box"><img key={pos} 
                    src={smImage_url+movie.backdrop_path} alt="" className="poster" /> 
                    <h3 className="b-title">{movie.title}</h3> 
-                   <p className="movie-desc">{movie.overview.split(" ").splice(0,40).join(" ") + '..'}</p> 
-                   </div> 
+                   <p className="movie-desc">{movie.overview.split(" ").splice(0,40).join(" ") + '..'} <br/>
+                   </p>                  
+                  </div> 
                    ) : movies.map((movie,pos)=>
-                   <div className="poster-box">
+                   <div onClick={()=>handleMovieClick(movie.id)} className="poster-box">
                        <img key={pos} src={smImage_url+movie.poster_path} alt="" className="poster" /> 
                        <h3 className="b-title">{movie.title.split(" ").splice(0,4).join(" ") }</h3> 
                        <p className="movie-desc">{movie.overview.split(" ").splice(0,20).join(" ") + '..'}
                        </p> 
                        </div> 
-                   ) 
-
-
-                    
+                   )                     
                    
                 }
                 
--- src/urls.js
+++ src/urls.js
@@ -1,4 +1,7 @@
-import { Api_Key } from "./Constants/Constants";
+import { Api_Key} from "./Constants/Constants";
+import axios from "./axios";
+
+
 
 
 export const originals = `discover/movie?api_key=${Api_Key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1&with_watch_providers=Netflix&with_watch_monetization_types=flatrate`;
@@ -7,3 +10,11 @@ export function more(pgnum)
     {
         return `discover/movie?api_key=${Api_Key}&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=${pgnum}&with_watch_providers=Amazon&with_watch_monetization_types=flatrate`
     }
+
+
+
+
+
+
+
+
